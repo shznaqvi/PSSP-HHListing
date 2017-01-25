2fc784fa1896070dae1a4620d6a80b4718d8822b
aliazaz <openmind_ali@hotmail.com>
2017-01-25 11:33:31 +0500

    Update SyncForm

--- app/src/main/java/edu/aku/hassannaqvi/pssp_hhlisting/SyncForms.java
+++ app/src/main/java/edu/aku/hassannaqvi/pssp_hhlisting/SyncForms.java
@@ -54,16 +54,14 @@ public class SyncForms extends AsyncTask<Void, Void, String> {
 
     }
 
-
-    @Override
-    protected String doInBackground(Void... params) {
-
+    private String downloadUrl(String myurl) throws IOException {
         String line = "No Response";
 
         HttpURLConnection connection = null;
         try {
-            String request = AppMain._IP + "/forms/";
+            String request = myurl;
             //String request = "http://10.1.42.30:3000/forms";
+            pd.setTitle("Connecting to... " + request);
             //pd.show();
             URL url = new URL(request);
             connection = (HttpURLConnection) url.openConnection();
@@ -91,7 +89,7 @@ public class SyncForms extends AsyncTask<Void, Void, String> {
 
             }
             wr.writeBytes(jsonSync.toString().replace("\uFEFF", "") + "\n");
-            //longInfo(jsonSync.toString().replace("\uFEFF", "") + "\n");
+            longInfo(jsonSync.toString().replace("\uFEFF", "") + "\n");
             wr.flush();
             int HttpResult = connection.getResponseCode();
             if (HttpResult == HttpURLConnection.HTTP_OK) {
@@ -128,6 +126,16 @@ public class SyncForms extends AsyncTask<Void, Void, String> {
                 connection.disconnect();
         }
         return line;
+    }
+
+
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            return downloadUrl(AppMain._IP + "/forms/");
+        } catch (IOException e) {
+            return "Unable to upload data. Server may be down.";
+        }
     }
 
     @Override
