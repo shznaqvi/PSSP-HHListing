730e19f9d225ff2210559f9a82e031a3ee5592ab
ali.azaz <openmind_ali@hotmail.com>
2018-01-09 18:26:12 +0500

    TOIC listing: app finishing phase CONTD..

---
+++ app/src/main/java/edu/aku/hassannaqvi/toic_hhlisting/Get/GetPSUs.java
@@ -0,0 +1,110 @@
+package edu.aku.hassannaqvi.toic_hhlisting.Get;
+
+/**
+ * Created by hassan.naqvi on 09/01/2018.
+ */
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import edu.aku.hassannaqvi.toic_hhlisting.Core.AppMain;
+import edu.aku.hassannaqvi.toic_hhlisting.Contracts.PSUsContract;
+import edu.aku.hassannaqvi.toic_hhlisting.Core.FormsDBHelper;
+
+public class GetPSUs extends AsyncTask<String, String, String> {
+
+    private final String TAG = "GetPSUs()";
+    HttpURLConnection urlConnection;
+    private Context mContext;
+    private ProgressDialog pd;
+
+    public GetPSUs(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd = new ProgressDialog(mContext);
+        pd.setTitle("Syncing PSUs");
+        pd.setMessage("Getting connected to server...");
+        pd.show();
+
+    }
+
+    @Override
+    protected String doInBackground(String... args) {
+
+        StringBuilder result = new StringBuilder();
+
+        URL url = null;
+        try {
+            url = new URL(AppMain._HOST_URL + PSUsContract.singlePSU._URI);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            Log.d(TAG, "doInBackground: " + urlConnection.getResponseCode());
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Log.i(TAG, "PSUs In: " + line);
+                    result.append(line);
+                }
+            }
+        } catch (java.net.SocketTimeoutException e) {
+            return null;
+        } catch (java.io.IOException e) {
+            return null;
+        } finally {
+            urlConnection.disconnect();
+        }
+
+
+        return result.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        //Do something with the JSON string
+
+        if (result != null) {
+            String json = result;
+            if (json.length() > 0) {
+                FormsDBHelper db = new FormsDBHelper(mContext);
+                try {
+                    JSONArray jsonArray = new JSONArray(json);
+                    db.syncPSU(jsonArray);
+                    pd.setMessage("Received: " + jsonArray.length());
+                    pd.show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                pd.setMessage("Received: " + json.length() + "");
+                pd.show();
+            }
+        } else {
+            pd.setTitle("Connection Error");
+            pd.setMessage("Server not found!");
+            pd.show();
+        }
+    }
+}
