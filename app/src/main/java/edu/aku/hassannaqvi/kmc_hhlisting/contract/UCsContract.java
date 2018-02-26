520af111d3556dadfbd8c2279c9089457862dcfc
gul.sanober <gulsanobermemon@gmail.com>
2018-02-26 18:43:15 +0500

    kmc changes

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_hhlisting/contract/UCsContract.java
@@ -0,0 +1,77 @@
+package edu.aku.hassannaqvi.kmc_hhlisting.contract;
+
+import android.database.Cursor;
+import android.provider.BaseColumns;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by hassan.naqvi on 10/31/2016.
+ */
+
+public class UCsContract {
+
+    private static String TAG = "";
+
+    private String ucCode;
+    private String ucName;
+    private String districtCode;
+
+    public UCsContract() {
+    }
+
+    public UCsContract sync(JSONObject jsonObject) throws JSONException {
+
+        this.ucCode = jsonObject.getString(singleUC.COLUMN_UC_CODE);
+        this.ucName = jsonObject.getString(singleUC.COLUMN_UC_NAME);
+        this.districtCode = jsonObject.getString(singleUC.COLUMN_DISTRICT_CODE);
+
+        return this;
+    }
+
+    public UCsContract hydrate(Cursor cursor) {
+        this.ucCode = cursor.getString(cursor.getColumnIndex(singleUC.COLUMN_UC_CODE));
+        this.ucName = cursor.getString(cursor.getColumnIndex(singleUC.COLUMN_UC_NAME));
+        this.districtCode = cursor.getString(cursor.getColumnIndex(singleUC.COLUMN_DISTRICT_CODE));
+
+        return this;
+    }
+
+    public String getDistrictCode() {
+        return districtCode;
+    }
+
+    public void setDistrictCode(String districtCode) {
+        this.districtCode = districtCode;
+    }
+
+    public String getUcCode() {
+        return ucCode;
+    }
+
+    public void setUcCode(String ucCode) {
+        this.ucCode = ucCode;
+    }
+
+    public String getUcName() {
+        return ucName;
+    }
+
+    public void setUcName(String ucName) {
+        this.ucName = ucName;
+    }
+
+    public static abstract class singleUC implements BaseColumns {
+
+        public static final String TABLE_NAME = "UCs";
+        public static final String COLUMN_NAME_NULLABLE = "nullColumnHack";
+        public static final String _ID = "_ID";
+        public static final String COLUMN_UC_CODE = "uc_code";
+        public static final String COLUMN_UC_NAME = "uc_name";
+        public static final String COLUMN_DISTRICT_CODE = "taluka_code";
+
+        public static final String _URI = "ucs.php";
+    }
+
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_hhlisting/ui/LoginActivity.java
@@ -0,0 +1,304 @@
+package edu.aku.hassannaqvi.kmc_hhlisting.ui;
+
+import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import edu.aku.hassannaqvi.kmc_hhlisting.R;
+import edu.aku.hassannaqvi.kmc_hhlisting.core.AppMain;
+import edu.aku.hassannaqvi.kmc_hhlisting.core.FormsDBHelper;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor> {
+
+
+
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "test1234:test1234", "testS12345:testS12345", "bar@example.com:world"};
+    public ArrayList<String> lables;
+    public ArrayList<String> values;
+    @BindView(R.id.email_sign_in_button)
+    Button email_sign_in_button;
+    @BindView(R.id.activity_login)
+    LinearLayout activityLogin;
+    @BindView(R.id.login_progress)
+    ProgressBar mProgressView;
+    @BindView(R.id.login_form)
+    ScrollView loginForm;
+    @BindView(R.id.email_login_form)
+    LinearLayout emailLoginForm;
+    @BindView(R.id.email)
+    AutoCompleteTextView mEmailView;
+    @BindView(R.id.password)
+    EditText mPasswordView;
+    @BindView(R.id.txtinstalldate)
+    TextView txtinstalldate;
+    private UserLoginTask mAuthTask = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        populateAutoComplete();
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.email_sign_in_button) {
+            attemptLogin();
+        }
+    }
+
+    private void populateAutoComplete() {
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } /*else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }*/
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() >= 7;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+    public void gotoMain(View v) {
+        Intent im = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(im);
+
+    }
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+
+            LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                FormsDBHelper db = new FormsDBHelper(LoginActivity.this);
+                if ((mEmail.equals("dmu@aku") && mPassword.equals("aku?dmu")) || (mEmail.equals("test1234") && mPassword.equals("test1234")) || db.Login(mEmail, mPassword)) {
+                    AppMain.username = mEmail;
+//                    MainApp.admin = mEmail.contains("@");
+
+                    Intent iLogin = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(iLogin);
+
+                } else {
+                    mPasswordView.setError(getString(R.string.error_incorrect_password));
+                    mPasswordView.requestFocus();
+                    Toast.makeText(LoginActivity.this, mEmail + " " + mPassword, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        LoginActivity.this);
+                alertDialogBuilder
+                        .setMessage("GPS is disabled in your device. Enable it?")
+                        .setCancelable(false)
+                        .setPositiveButton("Enable GPS",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        Intent callGPSSettingIntent = new Intent(
+                                                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                        startActivity(callGPSSettingIntent);
+                                    }
+                                });
+                alertDialogBuilder.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = alertDialogBuilder.create();
+                alert.show();
+
+            }
+
+        }
+
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+        }
+    }
+
+
+}
+
+
