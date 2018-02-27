b47668ad2ec9b0683402926b725b534af849bebf
shznaqvi <shznaqvi@gmail.com>
2018-02-27 21:45:19 +0500

    KMCNew

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_hhlisting2/ui/SplashScreen.java
@@ -0,0 +1,27 @@
+package edu.aku.hassannaqvi.kmc_hhlisting2.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.kmc_hhlisting2.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
