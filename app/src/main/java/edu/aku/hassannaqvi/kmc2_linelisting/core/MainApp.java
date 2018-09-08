2a9598965cfee821015d7d76f8e8b31c6a15ad4d
hassan.naqvi <shznaqvi@gmail.com>
2018-09-08 12:43:17 +0500

    New Branch Modifications TMK to KMC2

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc2_linelisting/core/MainApp.java
@@ -0,0 +1,230 @@
+package edu.aku.hassannaqvi.kmc2_linelisting.core;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Map;
+
+import edu.aku.hassannaqvi.kmc2_linelisting.Other.TypefaceUtil;
+import edu.aku.hassannaqvi.kmc2_linelisting.contracts.ListingContract;
+
+/**
+ * Created by hassan.naqvi on 10/15/2016.
+ */
+
+public class MainApp extends Application {
+
+    // GPS Related Field Variables
+
+    //public static String _IP = "10.1.42.30"; // Testing Server
+    public static final String _IP = "43.245.131.159"; // Test PHP server
+    public static final Integer _PORT = 8080; // Port - with colon (:)
+    private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1; // in Meters
+    private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000; // in Milliseconds
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+    public static String _HOST_URL = "http://" + MainApp._IP + ":" + MainApp._PORT + "/tmk/api/";
+
+    public static String TAG = "MainApp";
+    public static ListingContract lc;
+    public static String hh01txt = "0000";
+    public static String hh02txt;
+    public static int hh03txt = 0;
+    public static String hh07txt;
+    public static int fCount = 0;
+    public static int fTotal = 0;
+    public static int cCount = 0;
+    public static int hh07 = 0;
+    public static int cTotal = 0;
+    public static SharedPreferences sharedPref;
+    public static String userEmail;
+    protected static LocationManager locationManager;
+    Location location;
+
+//    Ali
+
+    public static int ucCode = 0;
+    public static int talukaCode = 0;
+    public static int areaCode = 0;
+    public static String villageCode = "";
+    public static String villageName = "";
+
+    public static void updatePSU(String psuCode, String structureNo) {
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(psuCode, structureNo);
+
+        editor.apply();
+        Log.d(TAG, "updatePSU: " + psuCode + " " + structureNo);
+
+    }
+
+    public static Boolean PSUExist(String psuCode) {
+        Log.d(TAG, "PSUExist: " + psuCode);
+        MainApp.hh03txt = Integer.valueOf(sharedPref.getString(psuCode, "0"));
+        Log.d(TAG, "PSUExist (Test): " + sharedPref.getString(psuCode, "0"));
+
+        if (MainApp.hh03txt == 0) {
+            Log.d(TAG, "PSUExist (False): " + MainApp.hh03txt);
+
+            return false;
+        } else {
+            Log.d(TAG, "PSUExist (True): " + MainApp.hh03txt);
+
+            return true;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("App", "Creating...");
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/JameelNooriNastaleeq.ttf"); // font from assets: "assets/fonts/Roboto-Regular.ttf
+
+        // Declare and Initialize GPS collection module
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MINIMUM_TIME_BETWEEN_UPDATES,
+                MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
+                new MyLocationListener()
+        );
+
+        sharedPref = getSharedPreferences("PSUCodes", Context.MODE_PRIVATE);
+
+    }
+
+    protected void showCurrentLocation() {
+
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            String message = String.format(
+                    "Current Location \n Longitude: %1$s \n Latitude: %2$s",
+                    location.getLongitude(), location.getLatitude()
+            );
+            //Toast.makeText(getApplicationContext(), message,
+            //Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void showGPSCoordinates(View v) {
+        showCurrentLocation();
+
+
+    }
+
+    /**
+     * Checks whether two providers are the same
+     */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else return isNewer && !isSignificantlyLessAccurate && isFromSameProvider;
+    }
+
+    private class MyLocationListener implements LocationListener {
+
+        public void onLocationChanged(Location location) {
+
+            SharedPreferences sharedPref = getSharedPreferences("GPSCoordinates", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+
+            String dt = DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(sharedPref.getString("Time", "0"))).toString();
+
+            Location bestLocation = new Location("storedProvider");
+            bestLocation.setAccuracy(Float.parseFloat(sharedPref.getString("Accuracy", "0")));
+            bestLocation.setTime(Long.parseLong(sharedPref.getString(dt, "0")));
+//                bestLocation.setTime(Long.parseLong(dt));
+            bestLocation.setLatitude(Float.parseFloat(sharedPref.getString("Latitude", "0")));
+            bestLocation.setLongitude(Float.parseFloat(sharedPref.getString("Longitude", "0")));
+
+            if (isBetterLocation(location, bestLocation)) {
+                editor.putString("Longitude", String.valueOf(location.getLongitude()));
+                editor.putString("Latitude", String.valueOf(location.getLatitude()));
+                editor.putString("Accuracy", String.valueOf(location.getAccuracy()));
+                editor.putString("Time", String.valueOf(location.getTime()));
+//                    editor.putString("Time", DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(String.valueOf(location.getTime()))).toString());
+
+//                String date = DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(String.valueOf(location.getTime()))).toString();
+//                Toast.makeText(getApplicationContext(),
+//                        "GPS Commit! LAT: " + String.valueOf(location.getLongitude()) +
+//                                " LNG: " + String.valueOf(location.getLatitude()) +
+//                                " Accuracy: " + String.valueOf(location.getAccuracy()) +
+//                                " Time: " + date,
+//                        Toast.LENGTH_SHORT).show();
+
+                editor.apply();
+            }
+
+
+            Map<String, ?> allEntries = sharedPref.getAll();
+            for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+                Log.d("Map", entry.getKey() + ": " + entry.getValue().toString());
+            }
+        }
+
+        public void onStatusChanged(String s, int i, Bundle b) {
+            showCurrentLocation();
+        }
+
+        public void onProviderDisabled(String s) {
+
+        }
+
+        public void onProviderEnabled(String s) {
+
+        }
+
+    }
+
+}
